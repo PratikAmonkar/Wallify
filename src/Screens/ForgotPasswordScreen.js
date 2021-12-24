1b38b12f64b64cbc75150c7b2531b4236bc1043c
alpha-3000 <amonkar3000@gmail.com>
2021-12-24 09:21:30 +0530

    change in ui of forgot password screen

--- src/Screens/ForgotPasswordScreen.js
+++ src/Screens/ForgotPasswordScreen.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import forgotPasswordImg from "../assets/images/undraw_forgot_password_re_hxwm.svg";
 
 const ForgotPasswordScreen = () => {
   return (
     <>
-      <div className="container pt-lg-4">
+      <div className="container pt-4">
         <div className=" align-items-center text-center ">
-          <h2 className="mb-0">Welcome</h2>
+          <h2 className="mb-0">Forgot password</h2>
         </div>
-        <div className="row align-items-center py-5 mt-lg-4">
-          <div className="col-md-10 col-lg-6 mx-auto mt-3">
+        <div className="row py-5 mt-lg-4 mt-sm-0 align-items-center">
+          <div className="col-lg-5 col-md-12 pr-lg-5 text-center">
+            <img
+              src={forgotPasswordImg}
+              alt=""
+              className="img-fluid mt-3 d-none d-lg-block"
+            />
+          </div>
+          <div className="col-lg-6 col-md-10 mx-auto mt-4 ">
             <form>
               <div className="row">
                 <div className="input-group col-lg-12 mb-4">
@@ -22,26 +29,20 @@ const ForgotPasswordScreen = () => {
                     autoComplete="off"
                   />
                 </div>
-                <div className="input-group col-lg-12 mb-4">
+                <div className="input-group col-md-12 col-lg-12 mb-4">
                   <input
                     id="confirmPassword"
                     type="password"
-                    name="password"
+                    name="confirmPassword"
                     placeholder="Enter confirm password"
                     className="form-control bg-white"
                     autoComplete="off"
                   />
                 </div>
-                <div className="button_div d-inline-block ml-3">
-                  <div className="d-flex">
-                    <Link
-                      className={`btn btn-style w-100`}
-                      type="submit"
-                      to="/"
-                    >
-                      Change
-                    </Link>
-                  </div>
+                <div className="form-group col-lg-12 mx-auto mb-0 btn-style1">
+                  <button type="button" className="btn btn-primary btn-style1">
+                    Submit
+                  </button>
                 </div>
               </div>
             </form>
